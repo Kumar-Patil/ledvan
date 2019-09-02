46a8e70a302bc9366fac2a469a8e69665bb2529a
Jaganathan <jp@bmc.com>
2019-09-02 20:21:43 +0530

    un reviewed display

--- src/app/dashboard/led-vehicle/reviewed-display/reviewed-display.component.ts
+++ src/app/dashboard/led-vehicle/reviewed-display/reviewed-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LedVehicleService } from '../led-vehicle.service';
 
 @Component({
   selector: 'app-reviewed-display',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reviewed-display.component.css']
 })
 export class ReviewedDisplayComponent implements OnInit {
-
-  constructor() { }
+  cols: any[];
+  users: any[];
+  constructor(private ledVehicleService: LedVehicleService) { }
 
   ngOnInit() {
+    this.ledVehicleService.getReviewedDisplays().subscribe(res => {
+      this.users = res;
+    });
+    this.cols = [
+        { field: 'reportDate', header: 'Date' },
+        { field: 'area', header: 'Area' },
+        { field: 'vehicle', header: 'Vehicle No' },
+        { field: 'displayTime', header: 'Display Time' },
+        { field: 'review', header: 'Reviews' },
+    ];
   }
 
 }
--- src/app/dashboard/led-vehicle/unreviewed-display/unreviewed-display.component.ts
+++ src/app/dashboard/led-vehicle/unreviewed-display/unreviewed-display.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
-
+import {LedVehicleService} from '../led-vehicle.service';
 @Component({
     selector: 'app-unreviewed-display',
     templateUrl: './unreviewed-display.component.html',
@@ -11,35 +11,32 @@ export class UnreviewedDisplayComponent implements OnInit {
     users: User[];
 
     cols: any[];
-    ngOnInit() {
-        this.users = [
-            { id: '1', name: 'kiran', email: 'kiran@gmail.com' },
-            { id: '2', name: 'tom', email: 'tom@gmail.com' },
-            { id: '3', name: 'john', email: 'john@gmail.com' },
-            { id: '4', name: 'Frank', email: 'frank@gmail.com' },
-            { id: '5', name: 'kiran', email: 'kiran@gmail.com' },
-            { id: '6', name: 'tom', email: 'tim@gmail.com' },
-            { id: '7', name: 'john', email: 'fun@gmail.com' },
-            { id: '8', name: 'Frank', email: 'gun@gmail.com' },
-            { id: '9', name: 'kiran', email: 'run@gmail.com' },
-            { id: '10', name: 'tom', email: 'gin@gmail.com' },
-            { id: '11', name: 'john', email: 'kin@gmail.com' },
-            { id: '12', name: 'Frank', email: 'tin@gmail.com' },
-            { id: '13', name: 'kiran', email: 'kiue@gmail.com' },
-            { id: '14', name: 'tom', email: 'fff@gmail.com' },
-            { id: '15', name: 'john', email: 'surf@gmail.com' },
-            { id: '16', name: 'Frank', email: 'rnk@gmail.com' },
-            { id: '17', name: 'kiran', email: 'riran@gmail.com' },
-            { id: '18', name: 'tom', email: 'tomuus@gmail.com' },
-            { id: '19', name: 'john', email: 'joen@gmail.com' },
-            { id: '20', name: 'Frank', email: 'fanker@gmail.com' },
 
-        ];
+    constructor(private ledVehicleService: LedVehicleService) {
+
+    }
+    ngOnInit() {
+
+        this.ledVehicleService.getUnreviewedDisplays().subscribe(res => {
+            this.users = res;
+        });
         this.cols = [
-            { field: 'id', header: 'Id' },
-            { field: 'name', header: 'Name' },
-            { field: 'email', header: 'Email' },
+            { field: 'reportDate', header: 'Date' },
+            { field: 'area', header: 'Area' },
+            { field: 'vehicle', header: 'Vehicle No' },
+            { field: 'displayTime', header: 'Display Time' },
+            { field: 'delete', header: '' },
         ];
+    }
+
+    /**
+     * delete
+     */
+    public delete(user) {
+        console.log('About to delete an user: ' + JSON.stringify(user));
+        this.ledVehicleService.deleteUser(user).subscribe(res => {
+            // success block delete user
+        });
     }
 }
 
